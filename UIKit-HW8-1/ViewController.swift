e4f3f1f45ece73d1a6018b6374213b2203975afe
Varyzgin <dima5varyzgin@gmail.com>
2024-12-10 12:36:00 +0300

    Project created

---
+++ UIKit-HW8-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-HW8-1
+//
+//  Created by Дима on 12/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
